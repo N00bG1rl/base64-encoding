277d0d1a0b46594d60fc3c0e962fe6c892f44958
N00bG1rl <marttila.helen@gmail.com>
2021-12-23 09:58:38 +0200

    Basic done

--- app.js
+++ app.js
@@ -1,5 +1,19 @@
+const { hash } = window.location
+
+const secret = atob(hash.replace('#', ''))
+
+if (secret) {
+  document.getElementById('form-panel').classList.add('hide')
+  document.getElementById('encoded-panel').classList.remove('hide')
+
+  document.getElementById('heading').innerHTML = secret
+}
+
 document.getElementById('form').addEventListener('submit', e => {
   e.preventDefault()
+
+  document.getElementById('form-panel').classList.add('hide')
+  document.getElementById('share-panel').classList.remove('hide')
 
   const input = document.getElementById('form-input')
   const encrypted = btoa(input.value)
